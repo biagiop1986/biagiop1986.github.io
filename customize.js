c3a6851bbb8dc372e2453a3aa94a55f9d4a7351c
biagiop1986 <biagiop1986@gmail.com>
2024-04-25 20:08:25 +0200

    CSS not loaded in 404 page - workaround with JS

--- customize.js
+++ customize.js
@@ -2,8 +2,10 @@ function removeContainer()
 {
     if($(window).width() < 991) {
       $('.no-pad-small-screen').removeClass('container');
+      $('.nav-margin').css( { marginLeft: 20 });
     } else {
       $('.no-pad-small-screen').addClass('container');
+      $('.nav-margin').css( { marginLeft: 0 });
     }
 }
 
