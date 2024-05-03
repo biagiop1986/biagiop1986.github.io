45905be6a4d1c2ed3cf7646475288857e8ddb95e
biagiop1986 <biagiop1986@gmail.com>
2024-05-03 23:09:33 +0200

    Added day-dependent color-theme

--- customize.js
+++ customize.js
@@ -9,5 +9,43 @@ function removeContainer()
     }
 }
 
-$(window).on('resize', removeContainer)
-$(window).on('load', removeContainer)
+function pickColorOfDay()
+{
+    const d = new Date();
+    const r = d.getDay();
+
+    if (r == 1 || r == 4) {
+      c1 = 'rgb(20, 54, 54)';
+      c2 = 'rgb(7, 64, 64)';
+      c3 = 'rgb(61, 94, 94)';
+    } else if (r == 2 || r == 5) {
+      c1 = 'rgb(26, 26, 70)';
+      c2 = 'rgb(10, 10, 90)';
+      c3 = 'rgb(70, 70, 108)';
+    } else if (r == 3 || r == 6) {
+      c1 = 'rgb(54, 20, 54)';
+      c2 = 'rgb(64, 7, 64)';
+      c3 = 'rgb(94, 61, 94)';
+    } else {
+      c1 = 'rgb(70, 26, 26)';
+      c2 = 'rgb(90, 10, 10)';
+      c3 = 'rgb(108, 70, 70)';
+    }
+
+    $('.dark-bg').css( { backgroundColor: c1 } );
+    $('b').css( { color: c2 } );
+    $('h3').css( { color: c2 } );
+    $('h4').css( { color: c2 } );
+    $('.custom-header').css( { color: c2 } );
+    $('.project-link-desc').css( { color: c2 } );
+    $('.unisi').css( { color: c3 } );
+}
+
+function onLoad()
+{
+    removeContainer();
+    pickColorOfDay();
+}
+
+$(window).on('resize', removeContainer);
+$(window).on('load', onLoad);
